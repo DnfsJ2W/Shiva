afb910618997ab8ae24ca3f80a0cbc224e487b76
ShivaKumarM999 <81612983+ShivaKumarM999@users.noreply.github.com>
2021-04-13 02:49:50 +0530

    Day 11 & Day 12 Commits
    Route Validations and Constraints in Web API.
    Token Based Authentication
    
    Day 12
    Security - Authorization
    Authorization using Roles
    
    Day 13 Morning Session
    Security Authorization - Concepts of  Areas, Font Awesome icon using Partial view in Areas.

---
+++ Day 12 & Morning Session of Day13/SecurityAuthorizationDemo/SecurityAuthorizationDemo/Controllers/DemoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SecurityAuthorizationDemo.Controllers
+{
+    [Authorize]
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Users ="Shiva@gmai.com")]
+        public ActionResult Page1()
+        {
+            return View();
+        }
+
+        public ActionResult Page2()
+        {
+            return View();
+        }
+    }
+}
---
+++ Day 12 & Morning Session of Day13/SecurityAuthorizationDemo/SecurityAuthorizationDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SecurityAuthorizationDemo.Startup))]
+namespace SecurityAuthorizationDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
