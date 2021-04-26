b6bc5deac799ea8f1e5d76db7842cea112cf99bd
ShivaKumarM999 <81612983+ShivaKumarM999@users.noreply.github.com>
2021-04-26 16:42:16 +0530

    React examples
    React examples

---
+++ Day 23/reactapp/src/components/counter.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';   
+import reactDom from 'react-dom';
+class Counter extends Component {
+    
+    render() { 
+        /* return <div><h1>Hello world</h1><button>Increment</button></div>; */
+        return <React.Fragment>
+            <h1>Hello world</h1>
+            <button>Increment1</button>
+        </React.Fragment>
+    }
+}
+ 
+export default Counter;
